a857b5f9712abc85a687fbe7a2b5e0ddb73ad3f1
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-05-19 08:18:36 +0530

    Operation management first commit

---
+++ src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,11 @@
+
+const Navbar = () =>{
+    return(
+        <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+            {/* Search bar */}
+        </div>
+    )
+}
+
+
+export default Navbar;
